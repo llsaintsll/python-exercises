c4e150b7c229b1ebe072f1be605c77b7fbd847cb
Santiago <lucassantiago1270@gmail.com>
2020-10-20 16:40:00 -0300

    Update exerc-3.py

--- Estrutura Sequencial/exerc-3.py
+++ Estrutura Sequencial/exerc-3.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça dois números e imprima a soma.
+
+a = int(input("Diga o primeiro número: "))
+b = int(input("Diga o segundo número: "))
+
+
+soma = a+b
+
+print("A soma foi: ", soma)
