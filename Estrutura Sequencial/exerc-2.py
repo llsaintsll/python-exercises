6208e906ec101a23f1df2857349e0a9016dab1cb
Santiago <lucassantiago1270@gmail.com>
2020-10-20 16:39:55 -0300

    Update exerc-2.py

--- Estrutura Sequencial/exerc-2.py
+++ Estrutura Sequencial/exerc-2.py
@@ -1,1 +1,5 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+# Faça um Programa que peça um número e então mostre a mensagem
+# O número informado foi [número].
+
+numero = input("Informe o número: ")
+print("O número foi: ", numero)
